164bff1cbd7f0ddab14739de9ab840b14f7dd6c5
lucasquearis <lucasquearis@hotmail.com>
2021-10-25 20:57:12 -0300

    requisito 1 feito

---
+++ challenges/desafio1.js
@@ -0,0 +1,31 @@
+db.movies.aggregate(
+  [
+    {
+      $match: {
+        $and: [
+          {
+            "imdb.rating": {
+              $gte: 7,
+            },
+          },
+          {
+            genres: {
+              $nin: ["Crime", "Horror"],
+            },
+          },
+          {
+            rated: {
+              $in: ["PG", "G"],
+            },
+          },
+          {
+            languages: "English",
+          },
+          {
+            languages: "Spanish",
+          },
+        ],
+      },
+    },
+  ],
+);
