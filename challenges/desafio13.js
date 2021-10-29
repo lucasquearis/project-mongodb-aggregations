fa7553c1e45b8a70758e82f267a683958eeaa6f1
lucasquearis <lucasquearis@hotmail.com>
2021-10-28 22:36:20 -0300

    requisito 13 feito

---
+++ challenges/desafio13.js
@@ -0,0 +1,32 @@
+db.trips.aggregate(
+  [
+    {
+      $match: {
+        startTime: {
+          $gte: ISODate("2016-03-10T00:00:00"),
+          $lte: ISODate("2016-03-10T23:59:59"),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        duracaoMediaEmMinutos: {
+          $avg: {
+            $divide: [{
+              $subtract: ["$stopTime", "$startTime"],
+            }, 60000],
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        duracaoMediaEmMinutos: {
+          $ceil: "$duracaoMediaEmMinutos",
+        },
+      },
+    },
+  ],
+);
